725b3d1d008bbbe5dca9fc096108f751a3fd060d
Ronald Osure <ronald@pculture.org>
2023-03-13 11:51:15 +0300

    add language to save on auto-detection time, organize data to include word segments

--- app.py
+++ app.py
@@ -11,11 +11,22 @@ def init():
     global model
     model = whisperx.load_model("large", DEVICE)
 
+def extract_segments(segments_list):
+    resp = []
+    for seg in segments_list:
+        resp.append({
+            'start': seg['start'],
+            'end': seg['end'],
+            'text': seg['text']
+        })
+    return resp
+
 # Inference is ran for every server call
 # Reference your preloaded global model variable here.
 def inference(model_inputs:dict) -> dict:
     global model
     # Parse out your arguments
+    language = model.inputs.get("language", None)
     mp3BytesString = model_inputs.get('mp3BytesString', None)
     if mp3BytesString == None:
         return {'message': "No input provided"}
@@ -26,7 +37,7 @@ def inference(model_inputs:dict) -> dict:
         file.write(mp3Bytes.getbuffer())
     
     # Run the model
-    result = model.transcribe(audio_file_name)
+    result = model.transcribe(audio_file_name, language=language)
 
     # load alignment model and metadata
     model_a, metadata = whisperx.load_align_model(language_code=result["language"], device=DEVICE)
@@ -34,5 +45,9 @@ def inference(model_inputs:dict) -> dict:
     # align whisper output
     result_aligned = whisperx.align(result["segments"], model_a, metadata, audio_file_name, DEVICE)
     os.remove(audio_file_name)
-    return result_aligned["word_segments"]
+    response = {
+        'word_segments': result_aligned["word_segments"],
+        'paragraphs': extract_segments(result_aligned['segments'])
+    }
+    return response
 
