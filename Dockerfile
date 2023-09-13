665906de53c31d22c86389ab4bfb5793c2414f5b
Louis Saberhagen <115233399+LouisSaberhagen@users.noreply.github.com>
2023-09-13 20:26:50 +0200

    Retrofitted everything into a banana server

--- Dockerfile
+++ Dockerfile
@@ -17,17 +17,13 @@ ADD requirements.txt requirements.txt
 RUN pip3 install -r requirements.txt
 
 # We add the banana boilerplate here
-ADD server.py .
+ADD app.py .
 
 # Add your model weight files 
 # (in this case we have a python script)
 ADD download.py .
 RUN python3 download.py
 
-
-# Add your custom app code, init() and inference()
-ADD app.py .
-
 EXPOSE 8000
 
-CMD python3 -u server.py
+CMD python3 -u app.py
--- app.py
+++ app.py
@@ -1,16 +1,17 @@
+# Do not edit if deploying to Banana Serverless
+# This file is boilerplate for the http server, and follows a strict interface.
+
+# Instead, edit the init() and inference() functions in app.py
+
+from potassium import Potassium, Request, Response
 import torch
 import whisperx
 import os
+import subprocess
 import base64
 from io import BytesIO
 
-DEVICE = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
-# Init is ran on server startup
-# Load your model to GPU as a global variable here using the variable name "model"
-def init():
-    global model
-    model = whisperx.load_model("large", DEVICE)
-    print(f"CUDA available? {torch.cuda.is_available()}, device: {DEVICE}")
+app = Potassium("my_app")
 
 def extract_segments(segments_list):
     resp = []
@@ -22,33 +23,59 @@ def extract_segments(segments_list):
         })
     return resp
 
-# Inference is ran for every server call
-# Reference your preloaded global model variable here.
-def inference(model_inputs:dict) -> dict:
-    global model
-    # Parse out your arguments
+# @app.init runs at startup, and loads models into the app's context
+@app.init
+def init():
+    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+    model = whisperx.load_model("large", device)
+    print(f"CUDA available? {torch.cuda.is_available()}, device: {device}")
+   
+    context = {
+        "model": model
+    }
+
+    return context
+
+# @app.handler runs for every call
+@app.handler("/")
+def handler(context: dict, request: Request) -> Response:
+    prompt = request.json.get("prompt")
+    model = context.get("model")
+    outputs = model(prompt)
+    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+
+    try:
+        model_inputs = response.json.loads(request.json)
+    except:
+        model_inputs = request.json
+
     language = model_inputs.get("language", None)
     mp3BytesString = model_inputs.get('mp3BytesString', None)
     if mp3BytesString == None:
-        return {'message': "No input provided"}
-    
+        return response.json({'message': "No input provided"})
+
     mp3Bytes = BytesIO(base64.b64decode(mp3BytesString.encode("ISO-8859-1")))
     audio_file_name = "input.mp3"
     with open(audio_file_name,'wb') as file:
         file.write(mp3Bytes.getbuffer())
-    
+
     # Run the model
     result = model.transcribe(audio_file_name, language=language)
 
     # load alignment model and metadata
-    model_a, metadata = whisperx.load_align_model(language_code=result["language"], device=DEVICE)
-
-    # align whisper output
-    result_aligned = whisperx.align(result["segments"], model_a, metadata, audio_file_name, DEVICE)
+    model_a, metadata = whisperx.load_align_model(language_code=result["language"], device=device)
+    result_aligned = whisperx.align(result["segments"], model_a, metadata, audio_file_name, device)
     os.remove(audio_file_name)
+
     response = {
         'word_segments': result_aligned["word_segments"],
         'paragraphs': extract_segments(result_aligned['segments'])
     }
-    return response
 
+    return Response(
+        json = {"outputs": response}, 
+        status=200
+    )
+
+if __name__ == "__main__":
+    app.serve()
